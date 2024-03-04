94fb8c865638999af63ac8a5bc6ce79c540536c7
PaytonIrvin06 <pa070262@ucf.edu>
2024-03-03 23:58:19 -0500

    Update ECO4443Midterm.py

--- ECO4443Midterm.py
+++ ECO4443Midterm.py
@@ -7,24 +7,35 @@ Created on Mon Feb 26 13:25:49 2024
 
 
 from itertools import combinations
+import pandas as pd
 from pandas import read_csv
+from pandas import DataFrame
 from statsmodels.api import add_constant
 import numpy as np
 import statsmodels.api as sm
 from numpy.random import seed
 from math import log
-
+from math import tan
+from math import sqrt
 
 import sklearn
 from sklearn.model_selection import cross_val_score, cross_validate
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import seaborn as sns
 
 # Payton Desktop Filepath
 data = read_csv('C:/Users/Payton Irvin/Documents/UCF/ECO4443/Python/Data/mid_term_dataset.csv')
 
 
-# Creates a correlation matrix to get a feel for the data
+# Creates a heatmapped correlation matrix to get a feel for the data
 corr = data.corr()
+
+sns.heatmap(corr);
+heatmap = sns.heatmap(data.corr().round(1), annot=True)
+heatmap.set_title("Correlation Heatmap")
+print(heatmap)
+
 
 #scaling the dependent variable
 data['price'] = data['price']/1000
@@ -32,138 +43,94 @@ data['price'] = data['price']/1000
 
 # Creating variables to evaluate in competing models
 
-data['parcel_home_interaction'] = data.parcel_size*data.home_size
-data['parcel_home_ratio'] = data.parcel_size/data.home_size
+#data['parcel_home_interaction'] = data.parcel_size*data.home_size
+data['parcel_home_ratio'] = data.home_size/data.parcel_size
+data['bath_bed_ratio'] = data.baths/data.beds
+data['r_coord'] = sqrt(data.x_coord**2) + (data.y_coord**2)
 
 
-data['x_y_interaction'] = data['x_coord']*data['y_coord']
-
-        
-data['area_sqd'] = data.home_size**2
-data['area_cubed'] = data.home_size**3
-
-data['parcel_sqd'] = data.parcel_size**2
-data['parcel_cubed'] = data.parcel_size**3
-
-
-data['age_sqd'] = data.age**2
-
-data['cbd_lakes_interaction'] = data.dist_cbd*data.dist_lakes
-data['cbd_sqd'] = data.dist_cbd**2
-
-data['lakes_sqd'] = data.dist_lakes**2
-
-data['bed_bath_interaction'] = data.beds*data.baths
-data['bed_home_size_interaction'] = data.beds*data.home_size
-data['bath_home_size_interaction'] = data.home_size*data.baths
-data['bed_bath_home_size_interaction'] = data.home_size*data.baths*data.beds
-
-data['pool_home_size_interaction'] = data.home_size*data.pool
+#data['x_y_interaction'] = data['x_coord']*data['y_coord']
+#data['area_sqd'] = data.home_size**2
+#data['area_cubed'] = data.home_size**3
+#data['parcel_sqd'] = data.parcel_size**2
+#data['parcel_cubed'] = data.parcel_size**3
+#data['age_sqd'] = data.age**2
+#data['cbd_lakes_interaction'] = data.dist_cbd*data.dist_lakes
+#data['cbd_sqd'] = data.dist_cbd**2
+#data['lakes_sqd'] = data.dist_lakes**2
+#data['bed_bath_interaction'] = data.beds*data.baths
+#data['bed_home_size_interaction'] = data.beds*data.home_size
+#data['bath_home_size_interaction'] = data.home_size*data.baths
+#data['bed_bath_home_size_interaction'] = data.home_size*data.baths*data.beds
+#data['pool_home_size_interaction'] = data.home_size*data.pool
 
 # Creating dummies for year
 data['year_2000'] = 0
+data.year_2000[data['year'] == 2000] = 1
 data['year_2001'] = 0
+data.year_2001[data['year'] == 2001] = 1
 data['year_2002'] = 0
+data.year_2002[data['year'] == 2002] = 1
 data['year_2003'] = 0
+data.year_2003[data['year'] == 2003] = 1
 data['year_2004'] = 0
+data.year_2004[data['year'] == 2004] = 1
 data['year_2005'] = 0
-
-for i in range(0, len(data.year)):
-    if (data.year[i] == 2000):
-        data.year_2000[i] = 1
-    elif (data.year[i] == 2001):
-        data.year_2001[i] = 1
-    elif (data.year[i] == 2002):
-        data.year_2002[i] = 1
-    elif (data.year[i] == 2003):
-        data.year_2003[i] = 1
-    elif (data.year[i] == 2004):
-        data.year_2004[i] = 1
-    elif (data.year[i] == 2005):
-        data.year_2005[i] = 1
+data.year_2005[data['year'] == 2005] = 1
         
 # dummies for beds
         
 data['bed_1'] = 0
+data.bed_1[data['beds'] == 1] = 1
 data['bed_2'] = 0
+data.bed_2[data['beds'] == 2] = 1
 data['bed_3'] = 0
+data.bed_3[data['beds'] == 3] = 1
 data['bed_4'] = 0
+data.bed_4[data['beds'] == 4] = 1
 data['bed_5'] = 0
+data.bed_5[data['beds'] == 5] = 1
 data['bed_6'] = 0
+data.bed_6[data['beds'] == 6] = 1
 data['bed_7'] = 0
-data['bed_8'] = 0
-
-for i in range(0, len(data.beds)):
-    if (data.beds[i] == 1):
-        data.bed_1[i] = 1
-    elif (data.beds[i] == 2):
-        data.bed_2[i] = 1
-    elif (data.beds[i] == 3):
-        data.bed_3[i] = 1
-    elif (data.beds[i] == 4):
-        data.bed_4[i] = 1
-    elif (data.beds[i] == 5):
-        data.bed_5[i] = 1
-    elif (data.beds[i] == 6):
-        data.bed_6[i] = 1
-    elif (data.beds[i] == 7):
-        data.bed_7[i] = 1
-    elif (data.beds[i] == 8):
-        data.bed_8[i] = 1
+data.bed_7[data['beds'] == 7] = 1
 
 #dummies for baths
 
-data['bath_1'] = 0
-data['bath_15'] = 0
-data['bath_2'] = 0
-data['bath_25'] = 0
-data['bath_3'] = 0
-data['bath_35'] = 0
-data['bath_4'] = 0
-data['bath_45'] = 0
-data['bath_5'] = 0
-data['bath_55'] = 0
-data['bath_6'] = 0
-data['bath_65'] = 0
-data['bath_7'] = 0
-data['bath_75'] = 0
-data['bath_8'] = 0
-data['bath_9'] = 0
 
+data['bath_1']= 0
+data.bath_1[data['baths'] == 1] = 1
+data['bath_15']= 0
+data.bath_15[data['baths'] == 1.5] = 1
+data['bath_2']= 0
+data.bath_2[data['baths'] == 2] = 1
+data['bath_25']= 0
+data.bath_25[data['baths'] == 2.5] = 1
+data['bath_3']= 0
+data.bath_3[data['baths'] == 3] = 1
+data['bath_35']= 0
+data.bath_35[data['baths'] == 3.5] = 1
+data['bath_4']= 0
+data.bath_4[data['baths'] == 4] = 1
+data['bath_45']= 0
+data.bath_45[data['baths'] == 4.5] = 1
+data['bath_5']= 0
+data.bath_5[data['baths'] == 5] = 1
+data['bath_55']= 0
+data.bath_55[data['baths'] == 5.5] = 1
+data['bath_6']= 0
+data.bath_6[data['baths'] == 6] = 1
+data['bath_65']= 0
+data.bath_65[data['baths'] == 6.5] = 1
+data['bath_7']= 0
+data.bath_7[data['baths'] == 7] = 1
+data['bath_75']= 0
+data.bath_75[data['baths'] == 7.5] = 1
+data['bath_8']= 0
+data.bath_8[data['baths'] == 8] = 1
+data['bath_9']= 0
+data.bath_9[data['baths'] == 9] = 1
 
-for i in range(0, len(data.baths)):
-    if (data.baths[i] == 1):
-        data.bath_1[i] = 1
-    elif (data.baths[i] == 1.5):
-        data.bath_15[i] = 1
-    elif (data.baths[i] == 2):
-        data.bath_2[i] = 1
-    elif (data.baths[i] == 2.5):
-        data.bath_25[i] = 1
-    elif (data.baths[i] == 3):
-        data.bath_3[i] = 1
-    elif (data.baths[i] == 3.5):
-        data.bath_35[i] = 1
-    elif (data.baths[i] == 4):
-        data.bath_4[i] = 1
-    elif (data.baths[i] == 4.5):
-        data.bath_45[i] = 1
-    elif (data.baths[i] == 5):
-        data.bath_5[i] = 1
-    elif (data.baths[i] == 5.5):
-        data.bath_55[i] = 1
-    elif (data.baths[i] == 6):
-        data.bath_6[i] = 1
-    elif (data.baths[i] == 6.5):
-        data.bath_65[i] = 1
-    elif (data.baths[i] == 7):
-        data.bath_7[i] = 1
-    elif (data.baths[i] == 7.5):
-        data.bath_75[i] = 1
-    elif (data.baths[i] == 8):
-        data.bath_8[i] = 1
-    elif (data.baths[i] == 9):
-        data.bath_9[i] = 1
 
 
 #creating model variable combinations
@@ -207,3 +174,57 @@ print("Minimum Average Test MSE:", min_mse.round(3))
 for possibles, i in mse.items():
     if i == -min_mse:
         print("The Combination of Variables:", possibles)
+
+################################################################################
+#Trying a new model using polynomial features
+
+
+seed(1234)
+data = data.sample(len(data))
+
+x_combos = []
+for n in range(1, 21):
+    combos = combinations(['age', 'beds', 'baths', 'home_size', 'parcel_size', 'pool', 'bath_bed_ratio',\
+                           'bed_3', 'bed_5', 'bath_2', 'bath_3', 'bath_4', 'bath_45', 'bath_5',\
+                               'bath_55', 'bath_65', 'bath_75', 'x_coord', 'y_coord', 'parcel_home_ratio'], n)
+    x_combos.extend(combos)
+
+y = data['price']
+
+mse = {}
+
+for n in range(0, len(x_combos)):
+    for j in range(1, 3): 
+        combo_list = list(x_combos[n])
+        x = data[combo_list]
+        poly = PolynomialFeatures(j)
+        poly_x = poly.fit_transform(x)
+        model = LinearRegression()
+        cv_scores = cross_validate(model, poly_x, y, cv=10, scoring=('neg_mean_squared_error'))
+        mse[str(combo_list), j] = np.mean(cv_scores['test_score'])
+
+print("Outcomes from the Best Linear Regression Model:")
+min_mse = abs(max(mse.values()))
+print("Minimum Average Test MSE:", min_mse.round(2))
+for possibles, i in mse.items():
+    if i == -min_mse:
+        print("The Combination of Variables:", possibles)
+
+
+##############################################################################
+#Model 2
+x = data[['home_size', 'pool', 'year', 'age', 'parcel_home_ratio', 'dist_lakes', 'dist_cbd', 'x_coord', 'y_coord', 'bath_bed_ratio', 'bed_3']]
+poly = PolynomialFeatures(3)
+poly_x = poly.fit_transform(x)
+
+# Add variable names
+x = DataFrame(poly.fit_transform(x), columns=poly.get_feature_names_out(x.columns))
+y = data['price']
+
+best_model = sm.OLS(y, x)
+results = best_model.fit()
+print(results.summary())
+
+pred = results.predict(poly_x)
+mse_best_model2 = sum((data.price - pred)**2)/results.nobs
+mse_best_model2
